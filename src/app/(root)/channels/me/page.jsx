a5ee667d86e8a5c33542ed846205b20d10bae31f
leebhin <bini4039@gmail.com>
2024-09-02 20:18:11 +0900

    basic layout

--- src/app/(root)/channels/me/page.jsx
+++ src/app/(root)/channels/me/page.jsx
@@ -2,8 +2,14 @@ import styles from "./friends.module.css"
 
 export default function Friends() {
   return (
-    <div className={styles.Friends}>
-      
+    <div className={styles.friends}>
+      <html>
+        <head>
+          <title>Thiscord | 친구</title>
+        </head>
+      </html>
+
+      ddd
     </div>
   )
 }
--- src/app/(root)/layout.jsx
+++ src/app/(root)/layout.jsx
@@ -1,19 +1,11 @@
-import { Inter } from "next/font/google";
 import "../globals.css";
 import styles from "./layout.module.css";
 import Link from "next/link";
 
-const inter = Inter({ subsets: ["latin"] });
-
-export const metadata = {
-  title: "Thiscord",
-  description: "Thiscord is great for playing games and chilling with friends, or even building a worldwide community. Customize your own space to talk, play, and hang out.",
-};
-
 export default function RootLayout({ children }) {
   return (
     <html lang="ko">
-      <body className={inter.className}>
+      <body className={styles.body}>
         <header className={styles.header}>
           <div className={styles.headerWrap}>
 
