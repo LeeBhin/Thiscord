e73e3804d01fc8c8c10718290e78fb32ea0e3a53
leebhin <bini4039@gmail.com>
2024-09-03 19:43:41 +0900

    frends list ui done

--- src/Images.js
+++ src/Images.js
@@ -1,9 +1,17 @@
 import background from '@/../../public/images/background.png';
-import friends from '@/../../public/svgs/friends.svg'
+import friends from '@/../../public/svgs/friends.svg';
+import nitro from '@/../../public/svgs/nitro.svg';
+import shop from '@/../../public/svgs/shop.svg';
+import plus from '@/../../public/svgs/plus.svg';
+import icon from '@/../../public/svgs/icon.svg'
 
 const Images = {
   background,
-  friends
+  friends,
+  nitro,
+  shop,
+  plus,
+  icon
 };
 
-export default { Images, friends };
+export default { Images, friends, nitro, shop, plus, icon };
--- src/app/(root)/layout.jsx
+++ src/app/(root)/layout.jsx
@@ -1,10 +1,15 @@
+"use client";
+
 import "../globals.css";
 import styles from "./layout.module.css";
 import Link from "next/link";
-
+import { usePathname } from "next/navigation";
 import Images from "@/Images";
 
 export default function RootLayout({ children }) {
+
+  const currentPath = usePathname();
+
   return (
     <html lang="ko">
       <body className={styles.body}>
@@ -15,10 +20,12 @@ export default function RootLayout({ children }) {
               <div className={styles.channelBar} />
               <Link href="/channels/me">
                 <div className={`${styles.channel} ${styles.direct}`}>
-                  <div className={styles.serverImg} />
+                  <Images.icon className={styles.serverImg} />
                 </div>
               </Link>
             </div>
+
+            <div className={styles.barricade} />
 
           </div>
 
@@ -31,16 +38,45 @@ export default function RootLayout({ children }) {
 
           <div className={styles.friendsWrap}>
             <div className={styles.topNav}>
-              <Link href="/channels/me" className={styles.friendsLink}>
+              <Link
+                href="/channels/me"
+                className={`${styles.friendsLink} ${currentPath === '/channels/me' ? styles.friendsLinkActive : ''}`}
+              >
                 <Images.friends className={styles.icon} />
                 <span className={styles.iconTxt}>친구</span>
               </Link>
 
-              <Link href="/store" className={styles.friendsLink}>
+              <Link
+                href="/store"
+                className={`${styles.friendsLink} ${currentPath === '/store' ? styles.friendsLinkActive : ''}`}
+              >
+                <Images.nitro className={styles.icon} />
+                <span className={styles.iconTxt}>Nitro</span>
               </Link>
 
-              <Link href="/shop" className={styles.friendsLink}>
+              <Link
+                href="/shop"
+                className={`${styles.friendsLink} ${currentPath === '/shop' ? styles.friendsLinkActive : ''}`}
+              >
+                <Images.shop className={styles.icon} />
+                <span className={styles.iconTxt}>상점</span>
               </Link>
+            </div>
+
+            <div className={styles.directTxtWrap}>
+              <p>다이렉트 메시지</p>
+              <Images.plus className={styles.plus} />
+            </div>
+
+            <div className={styles.friends}>
+
+              <div className={`${styles.friendsLink} ${styles.friendProfile}`}>
+                <div className={styles.profileIcon} >
+                  <Images.icon className={styles.profileImg} />
+                </div>
+                <div className={styles.name}>이빈</div>
+              </div>
+
             </div>
           </div>
 
