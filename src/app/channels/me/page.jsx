e278ffd3cd5039e73d4360f923a16bfccc047278
leebhin <bini4039@gmail.com>
2024-09-04 12:12:06 +0900

    friendsheader

--- src/app/channels/me/page.jsx
+++ src/app/channels/me/page.jsx
@@ -1,5 +1,6 @@
 import Head from 'next/head';
 import styles from './friends.module.css';
+import Images from '@/Images';
 
 export default function Friends() {
   return (
@@ -9,8 +10,41 @@ export default function Friends() {
       </Head>
 
       <header className={styles.header}>
+        <div className={styles.headerWrap}>
 
-      </header>
-    </div>
+          <div className={styles.frinedsTitle}>
+            <div className={styles.friendSvg}>
+              <Images.friends />
+            </div>
+            <div className={styles.friendsTxt}>친구</div>
+          </div>
+
+          <div className={styles.barricade} />
+
+          <div className={styles.statusWrap}>
+            <div className={`${styles.all} ${styles.statusActive}`}>
+              모두
+            </div>
+
+            <div className={styles.ready}>
+              대기 중
+            </div>
+
+            <div className={styles.ready}>
+              추천
+            </div>
+
+            <div className={styles.ready}>
+              친구 추가하기
+            </div>
+
+          </div>
+        </div>
+      </header >
+
+      <div className={styles.body}>
+
+      </div>
+    </div >
   );
 }
