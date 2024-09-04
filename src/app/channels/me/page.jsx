44fd5b897262136b40c2cd425ee103b4983ff09f
leebhin <bini4039@gmail.com>
2024-09-04 09:55:24 +0900

    layout header color fixed

--- src/app/channels/me/page.jsx
+++ src/app/channels/me/page.jsx
@@ -1,21 +1,16 @@
-import Head from "next/head"
-import styles from "./friends.module.css"
-
+import Head from 'next/head';
+import styles from './friends.module.css';
 
 export default function Friends() {
   return (
     <div className={styles.friends}>
-      {/* <html>
-        <head>
-          <title>Thiscord | 친구</title>
-        </head>
-      </html> */}
-
       <Head>
         <title>Thiscord | 친구</title>
       </Head>
 
-      ddd
+      <header className={styles.header}>
+        
+      </header>
     </div>
-  )
+  );
 }
