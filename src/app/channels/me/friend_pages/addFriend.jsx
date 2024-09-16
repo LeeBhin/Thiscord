114cde4d716ba1f2cb816bd44af98af55e0296f0
leebhin <bini4039@gmail.com>
2024-09-16 19:17:03 +0900

    friend request  result

--- src/app/channels/me/friend_pages/addFriend.jsx
+++ src/app/channels/me/friend_pages/addFriend.jsx
@@ -4,17 +4,28 @@ import { useState } from 'react';
 import { add_friend } from '@/utils/api';
 
 export default function Add_Friend() {
-    const [friendId, setFriendId] = useState()
-    const [status, setStatus] = useState()
+    const [friendId, setFriendId] = useState();
+    const [status, setStatus] = useState();
+    const [err, setErr] = useState();
 
     const sendRequest = async () => {
         try {
             const friends = await add_friend(friendId);
-            setStatus(friends)
+            setStatus(friends.message);
+            setErr(false);
         } catch (error) {
-            console.log(error.message)
+            setStatus(error.message);
+            setErr(true);
         }
     };
+
+    const handleInput = (e) => {
+        setFriendId(e);
+        if (!e) {
+            setErr();
+            setStatus();
+        }
+    }
 
     return (
         <div className={styles.addFriendBody}>
@@ -25,10 +36,17 @@ export default function Add_Friend() {
                     <input
                         type="text"
                         placeholder='Thiscord 사용자명을 사용하여 친구를 추가할 수 있어요.'
-                        onChange={(e) => setFriendId(e.target.value)}
+                        onChange={(e) => handleInput(e.target.value)}
+                        className={`${err === true ? styles.inputErr : err === false ? styles.inputOk : ''}`}
                     />
                     <button className={styles.sendRequestBtn} onClick={() => sendRequest()}>친구 요청 보내기</button>
                 </div>
+                {status ? (<p
+                    className={`${styles.resultMSG} ${err === true ? styles.resultErr : err === false ? styles.resultOk : ''}`}
+                >
+                    {status}
+                </p>) : (<p className={styles.resultMSG2}>.</p>)}
+
             </div>
 
             <div className={styles.line} />
