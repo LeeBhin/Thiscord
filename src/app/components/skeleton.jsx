734662e7f31ed99b4c809ca76f4458a75ef9a242
LeeBhin <bini4039@naver.com>
2024-10-18 15:06:58 +0900

    skeleton laoding

--- src/app/components/skeleton.jsx
+++ src/app/components/skeleton.jsx
@@ -1,27 +1,71 @@
-import styles from "./skeleton.module.css";
+import React, { memo } from "react";
+import styles from "../channels/me/[userId]/dm.module.css";
+
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+const SkeletonMessage = memo(() => {
+  const contentWrapCount = getRandomInt(1, 4);
+  const picIncluded = Math.random() < 0.27;
+  const picPosition = getRandomInt(0, contentWrapCount + 1);
+
+  return (
+    <div className={styles["sk-message"]}>
+      <div className={styles["sk-profile"]} />
+
+      <div className={styles["sk-infoWrap"]}>
+        <div
+          className={styles["sk-name"]}
+          style={{ width: `${getRandomInt(100, 150)}px` }}
+        />
+
+        {picIncluded && picPosition === 0 && (
+          <div
+            className={styles["sk-pic"]}
+            style={{ height: `${getRandomInt(80, 300)}px` }}
+          />
+        )}
+
+        {[...Array(contentWrapCount)].map((_, wrapIndex) => (
+          <div key={wrapIndex} className={styles["sk-wrapIndex"]}>
+            <div className={styles["sk-contentWrap"]}>
+              {[...Array(getRandomInt(1, 8))].map((_, contentIndex) => (
+                <div
+                  key={contentIndex}
+                  className={styles["sk-content"]}
+                  style={{ width: `${getRandomInt(40, 100)}px` }}
+                />
+              ))}
+            </div>
+
+            {picIncluded && picPosition === wrapIndex + 1 && (
+              <div
+                className={styles["sk-pic"]}
+                style={{ height: `${getRandomInt(80, 300)}px` }}
+              />
+            )}
+          </div>
+        ))}
+
+        {picIncluded && picPosition === contentWrapCount + 1 && (
+          <div
+            className={styles["sk-pic"]}
+            style={{ height: `${getRandomInt(80, 300)}px` }}
+          />
+        )}
+      </div>
+    </div>
+  );
+});
 
 export default function Skeleton() {
   return (
-    <div className={styles.skeleton}>
-      <div className={styles.messages}>
-        <div className={styles.message}>
-          <div className={styles.profile} />
-
-          <div className={styles.infoWrap}>
-            <div className={styles.name} />
-            <div className={styles.contentWrap}>
-              <div className={styles.content} />
-              <div className={styles.content} />
-              <div className={styles.content} />
-            </div>
-            <div className={styles.contentWrap}>
-              <div className={styles.content} />
-              <div className={styles.content} />
-              <div className={styles.content} />
-            </div>
-            <dv className={styles.pic} />
-          </div>
-        </div>
+    <div className={styles["sk-skeleton"]}>
+      <div className={styles["sk-messages"]}>
+        {[...Array(8)].map((_, index) => (
+          <SkeletonMessage key={index} />
+        ))}
       </div>
     </div>
   );
